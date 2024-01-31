bc205f557163d743bb3c0d87ff999e636c9678e9
SamirMamede <anuarsamir@gmail.com>
2024-01-30 20:09:54 -0400

    test todo api, call endpoind and get task

---
+++ test_todo_api/test_todo_api.py
@@ -0,0 +1,31 @@
+import requests
+
+ENDPOINT = 'https://todo.pixegami.io/'
+
+def test_call_endpoint():
+    response = requests.get(ENDPOINT)
+    assert response.status_code == 200
+
+def test_create_task():
+    payload = {
+        "content": "test api",
+        "user_id": "teste-user-id",
+        "is_done": False
+    }
+    create_task_response = requests.put(ENDPOINT + "/create-task", json=payload)
+    assert create_task_response.status_code == 200
+
+    data = create_task_response.json()
+    print(data)
+
+    task_id = data['task']['task_id']
+    get_task_id_response = requests.get(ENDPOINT + f"/get-task/{task_id}")
+    assert get_task_id_response.status_code == 200
+
+    data_get_task_id_response = get_task_id_response.json()
+    print(data_get_task_id_response)
+    assert data_get_task_id_response['content'] == payload['content']
+    assert data_get_task_id_response['user_id'] == payload['user_id']
+
+def test_update_task():
+    pass
